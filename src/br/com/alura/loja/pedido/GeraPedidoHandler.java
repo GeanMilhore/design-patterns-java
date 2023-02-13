c5660ff4997754a862fd4f3d7f74eaa8a3df157c
Gean Milhore <gean.milhore@gmail.com>
2023-02-13 10:26:12 -0300

    #observer - adicionando acoes desacopladas que ouvem a modificacao e entao sao executadas

--- src/br/com/alura/loja/pedido/GeraPedidoHandler.java
+++ src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -1,18 +1,28 @@
 package br.com.alura.loja.pedido;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 public class GeraPedidoHandler {
 
-	// construtor com injecao de dependencias: repository, service, etc...
+	private List<AcaoAposGerarPedido> acoes;
+
+	public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+		this.acoes = acoes;
+	}
 
 	public void execute(GeraPedido dados) {
 		Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
 		Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
+		
+		EnviarEmailPedido email = new EnviarEmailPedido();
+		SalvarPedidoNoBancoDeDados salvar = new SalvarPedidoNoBancoDeDados();
 
-		System.out.println("Salvar pedido no banco de dados");
-		System.out.println("Enviar Email com dados do novo Pedido");
+		acoes.forEach(a -> a.executarAcao(pedido));
 	}
 }
