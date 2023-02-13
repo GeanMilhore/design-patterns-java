213a7b77fc23f602d0be72785fce559d1148e227
Gean Milhore <gean.milhore@gmail.com>
2023-02-12 22:04:27 -0300

    #state - gerenciamento de comportamentos atraves do estados do contexto/objeto

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -2,16 +2,38 @@ package br.com.alura.loja.orcamento;
 
 import java.math.BigDecimal;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 public class Orcamento {
 
 	private BigDecimal valor;
 	private int quantidadeItens;
+	private SituacaoOrcamento situacao;
 
 	public Orcamento(BigDecimal valor, int quantidadeItens) {
 		this.valor = valor;
 		this.quantidadeItens = quantidadeItens;
+		this.situacao = new EmAnalise();
 	}
- 
+
+	public void aplicarDescontoExtra() {
+		BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
+		this.valor = this.valor.subtract(valorDoDescontoExtra);
+	}
+	
+	public void aprovar() {
+		this.situacao.aprovar(this);
+	}
+	
+	public void reprovar() {
+		this.situacao.reprovar(this);
+	}
+	
+	public void finalizar() {
+		this.situacao.finalizar(this);
+	}
+
 	public BigDecimal getValor() {
 		return valor;
 	}
@@ -22,5 +44,13 @@ public class Orcamento {
 
 	public void setQuantidadeItens(int quantidadeItens) {
 		this.quantidadeItens = quantidadeItens;
+	}
+
+	public SituacaoOrcamento getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(SituacaoOrcamento situacao) {
+		this.situacao = situacao;
 	}
 }
